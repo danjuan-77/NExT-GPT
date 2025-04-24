6a39a5a59955d9a37ff31ef2c74653f317e5bf9e
danjuan-77 <youngertutu@gmail.com>
2025-04-24 16:30:42 +0800

    Update gitignore

--- model_download.py
+++ model_download.py
@@ -3,3 +3,5 @@ from huggingface_hub import snapshot_download
 
 
 snapshot_download(repo_id="lmsys/vicuna-7b-v1.5",repo_type="model",local_dir="pretrain_ckpt/vicuna-7b-v1.5")
+
+snapshot_download(repo_id="ChocoWu/nextgpt_7b_tiva_v0",repo_type="model",local_dir="checkpoints/nextgpt-7b-v1.5")
