1afe088699fe66410fc2074a53f0493344f3770a
danjuan-77 <youngertutu@gmail.com>
2025-04-27 14:54:47 +0800

    11

--- NExT-GPT-Lagacy/code/model/custom_sd.py
+++ NExT-GPT-Lagacy/code/model/custom_sd.py
@@ -26,7 +26,8 @@ from diffusers.models import AutoencoderKL, UNet2DConditionModel
 from diffusers.schedulers import KarrasDiffusionSchedulers
 from diffusers.utils import deprecate, is_accelerate_available, logging, replace_example_docstring
 from diffusers.utils.torch_utils import randn_tensor
-from diffusers.pipeline_utils import DiffusionPipeline
+# from diffusers.pipeline_utils import DiffusionPipeline
+from diffusers.pipelines import DiffusionPipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 
