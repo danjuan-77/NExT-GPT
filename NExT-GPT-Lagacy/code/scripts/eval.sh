d7e567c2aec4600cf75bdbb06667f31c02b1049d
danjuan-77 <youngertutu@gmail.com>
2025-05-01 16:14:04 +0800

    11

--- NExT-GPT-Lagacy/code/scripts/eval.sh
+++ NExT-GPT-Lagacy/code/scripts/eval.sh
@@ -5,4 +5,5 @@ CKPT=../ckpt/delta_ckpt/nextgpt/7b_tiva_v0
 
 python eval.py \
     --nextgpt_ckpt_path $CKPT \
-    --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA
+    --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA \
+    --freeze_lm
