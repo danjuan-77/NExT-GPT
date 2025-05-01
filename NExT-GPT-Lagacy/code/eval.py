be6cdebaee1ec5d5c79e8b3e3c8b075a39b1c431
danjuan-77 <youngertutu@gmail.com>
2025-05-01 16:15:06 +0800

    11

--- NExT-GPT-Lagacy/code/eval.py
+++ NExT-GPT-Lagacy/code/eval.py
@@ -362,11 +362,12 @@ def main():
         help="Path to the task folder containing data.json and media files"
     )
 
+    task_path = args.task_path
+    
     args = vars(parser.parse_args())
     args.update(load_config(args))
 
     
-    task_path = args.task_path
     task_name = f"L{task_path.rsplit('/', 1)[0][-1]}_{task_path.rsplit('/', 1)[-1]}"
     model_name = "nextgpt"
     save_prediction_json = f'/share/nlp/tuwenming/projects/HAVIB/eval/user_outputs/{model_name}/tasks/{task_name}.json'
