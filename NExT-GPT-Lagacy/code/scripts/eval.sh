39cf386da830ba138c51c6cafa8e1d3b6fa32226
danjuan-77 <youngertutu@gmail.com>
2025-05-05 20:42:24 +0800

    update

--- NExT-GPT-Lagacy/code/scripts/eval.sh
+++ NExT-GPT-Lagacy/code/scripts/eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 CKPT=../ckpt/delta_ckpt/nextgpt/7b_tiva_v0
 
 # Level 1
@@ -15,17 +15,17 @@ CKPT=../ckpt/delta_ckpt/nextgpt/7b_tiva_v0
 # python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVH --freeze_lm
 python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVL --freeze_lm
 # python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVM --freeze_lm
-python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVR --freeze_lm
+# python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVR --freeze_lm
 # python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAH --freeze_lm
-python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAR --freeze_lm
+# python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAR --freeze_lm
 
 # Level 4
 # python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVC --freeze_lm
-python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG --freeze_lm
+# python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG --freeze_lm
 # python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVQA --freeze_lm
 
 # Level 5
-python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVLG --freeze_lm
+# python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVLG --freeze_lm
 # python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVQA --freeze_lm
 
 # nohup bash scripts/eval.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_nextgpt_gpu0_$(date +%Y%m%d%H%M%S).log 2>&1 &
