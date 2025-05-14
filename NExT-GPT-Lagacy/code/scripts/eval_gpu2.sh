606986cee40b32eb489e1b3c0b5850d200341f69
danjuan-77 <youngertutu@gmail.com>
2025-05-14 16:14:15 +0800

    update

--- NExT-GPT-Lagacy/code/scripts/eval_gpu2.sh
+++ NExT-GPT-Lagacy/code/scripts/eval_gpu2.sh
@@ -2,6 +2,8 @@
 export CUDA_VISIBLE_DEVICES=2
 CKPT=../ckpt/delta_ckpt/nextgpt/7b_tiva_v0
 
-python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAH --freeze_lm
+python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_6/AVSQA_av --freeze_lm
+python eval.py --nextgpt_ckpt_path $CKPT --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_6/AVSQA_v --freeze_lm
+
 
 # nohup bash scripts/eval_gpu2.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_nextgpt_unimodal_gpu2_$(date +%Y%m%d%H%M%S).log 2>&1 &
